23ee25cb11207ba29b37982ff8c035cc7e25c4b9
Heliken <burshf@gmail.com>
2023-03-26 14:05:22 +0300

    add proxy config for shorter http request

---
+++ src/proxy.conf.js
@@ -0,0 +1,11 @@
+module.exports = {
+    "/api": {
+        target: "https://api.themoviedb.org/3/",
+        changeOrigin: true,
+        pathRewrite: {
+            "^/api" : ""
+        },
+        logLevel: "debug",
+        secure: false,
+    }
+}
