5810589cafa0b22259299b0afd8ec65bd42e70e4
Heliken <burshf@gmail.com>
2023-03-31 23:31:23 +0300

    media types

--- src/app/types/person.ts
+++ src/app/types/person.ts
@@ -1,5 +1,8 @@
+import { MediaType } from './media-type';
+
 export interface Person {
   id: number;
   photo: string | null;
   name: string;
+  mediaType: MediaType.Person;
 }
--- src/app/types/tv-show.ts
+++ src/app/types/tv-show.ts
@@ -1,7 +1,10 @@
+import { MediaType } from './media-type';
+
 export interface TvShow {
   id: number;
   poster: string | null;
   title: string;
+  mediaType: MediaType.Tv;
   releaseDate?: Date;
   rating?: number;
 }
