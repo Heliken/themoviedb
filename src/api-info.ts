7c6adc0fa85d0ddf83209c13f186a4ad4a481a2c
Heliken <burshf@gmail.com>
2023-04-01 22:34:08 +0300

    moved local-storage caching logic to separate service

--- src/api-info.ts
+++ src/api-info.ts
@@ -2,6 +2,6 @@ export const API_KEY = 'f63dfcbf89639cad03df6a5bac873bca';
 
 export const CONFIG_CACHE_KEY = 'API_CONFIG_RESPONSE';
 
-export const CONFIG_EXPIRATION_TIME = 3 * 60 * 60 * 1000; // 3 days
+export const LOCAL_STORAGE_EXPIRATION_TIME = 3 * 24 * 60 * 60 * 1000; // 3 days
 
 export const RESPONSE_PER_PAGE = 20;
