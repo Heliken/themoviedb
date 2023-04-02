0d3c8131c07a9f15963393bfd297d1dfa59756ef
Heliken <burshf@gmail.com>
2023-04-02 12:08:38 +0300

    complete ui-card component

--- src/app/components/ui-card/ui-card.module.ts
+++ src/app/components/ui-card/ui-card.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { UiCardComponent } from './ui-card.component';
 import { ImgSrcModule } from '../../pipes/img-src/img-src.module';
+import { RatingBadgeModule } from '../rating-badge/rating-badge.module';
 
 @NgModule({
   declarations: [UiCardComponent],
   exports: [UiCardComponent],
-  imports: [CommonModule, ImgSrcModule],
+  imports: [CommonModule, ImgSrcModule, RatingBadgeModule],
 })
 export class UiCardModule {}
