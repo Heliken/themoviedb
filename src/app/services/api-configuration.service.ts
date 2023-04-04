5a0161d9b60a924055f7d0a2666b9fe44c2ce378
Heliken <burshf@gmail.com>
2023-04-04 23:06:20 +0300

    rework apiConfigurationService to return observable

--- src/app/services/api-configuration.service.ts
+++ src/app/services/api-configuration.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { CONFIG_CACHE_KEY } from 'src/api-info';
 import { ConfigurationResponse } from '../types/api-configuration';
 import { LocalStorageService } from './local-storage.service';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,11 +18,12 @@ export class ApiConfigurationService {
     return this.config;
   }
 
-  public loadConfig(): void {
+  public loadConfig(): Observable<ConfigurationResponse | undefined> {
     if (this.cachedConfig) {
       this.config = this.cachedConfig;
+      return of(this.config);
     } else {
-      this.requestConfig();
+      return this.requestConfig().pipe(tap(data => this.saveConfig(data)));
     }
   }
 
@@ -29,10 +31,8 @@ export class ApiConfigurationService {
   private cachedConfig = this.localStorageService.getItem(this.cacheKey);
   private config?: ConfigurationResponse;
 
-  private requestConfig(): void {
-    this.http.get<ConfigurationResponse>('configuration').subscribe(data => {
-      this.saveConfig(data);
-    });
+  private requestConfig(): Observable<ConfigurationResponse> {
+    return this.http.get<ConfigurationResponse>('configuration');
   }
 
   private saveConfig(newConfig: ConfigurationResponse): void {
