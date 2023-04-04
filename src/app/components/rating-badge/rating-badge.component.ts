15fa7565798514bac188bae846a6b31ae945e4a0
Heliken <burshf@gmail.com>
2023-04-04 21:31:46 +0300

    rework rating value as a setter

--- src/app/components/rating-badge/rating-badge.component.ts
+++ src/app/components/rating-badge/rating-badge.component.ts
@@ -1,16 +1,20 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { getBgColor } from './helpers/rating-color';
 @Component({
   selector: 'mdb-rating-badge',
   templateUrl: './rating-badge.component.html',
   styleUrls: ['./rating-badge.component.scss'],
 })
-export class RatingBadgeComponent implements OnInit {
-  @Input() public value!: number;
-
+export class RatingBadgeComponent {
+  private _value = 0;
   public bgColor = 'rgb(0,0,0)';
 
-  public ngOnInit(): void {
-    this.bgColor = getBgColor(this.value);
+  @Input() public set value(value: number) {
+    this._value = value;
+    this.bgColor = getBgColor(value);
+  }
+
+  public get value() {
+    return this._value;
   }
 }
