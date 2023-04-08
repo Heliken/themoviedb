68c4d4859bdef3e23d25f7c3f9d1853b4d790e35
Heliken <burshf@gmail.com>
2023-04-08 20:48:05 +0300

    rework search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Observable, Subject, debounceTime, tap } from 'rxjs';
+import { Observable, Subject, debounceTime, filter, tap } from 'rxjs';
 import { SearchApiService } from './search-api.service';
 import { SearchResponse } from './types/search-response';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'mdb-search',
@@ -11,51 +12,27 @@ import { SearchResponse } from './types/search-response';
 export class SearchComponent implements OnInit {
   constructor(private searchApi: SearchApiService) {}
 
-  public search$ = new Subject<string>();
-  public results$ = new Observable<SearchResponse>();
+  public searchInput = new FormControl();
+  public searchResult?: SearchResponse;
   public debounceTime = 200;
   public minSearchLength = 3;
-  public hasMinInput = false;
-
-  public onInput(event: Event) {
-    if (event.target instanceof HTMLInputElement) {
-      const searchString = event.target.value.trim();
-      if (searchString.length >= this.minSearchLength) {
-        this.search$.next(event.target.value.trim());
-        this.showSearchOverlay();
-      } else {
-        this.hideSearchOverlay();
-      }
-    }
-  }
-
-  public showSearchOverlay() {
-    this.hasMinInput = true;
-  }
-
-  public hideSearchOverlay() {
-    this.hasMinInput = false;
-  }
 
   public clear() {
-    this.hasMinInput = false;
-  }
-
-  public onFocus(event: Event) {
-    if (event.target instanceof HTMLInputElement) {
-      const searchString = event.target.value.trim();
-      if (searchString.length >= this.minSearchLength) {
-        this.showSearchOverlay();
-      }
-    }
+    this.searchInput.setValue('');
+    this.searchResult = undefined;
   }
 
   public ngOnInit(): void {
-    this.search$
-      .pipe(debounceTime(this.debounceTime))
-      .subscribe(
-        searchString =>
-          (this.results$ = this.searchApi.requestSearchResult(searchString))
-      );
+    this.searchInput.valueChanges
+      .pipe(
+        tap(() => (this.searchResult = undefined)),
+        debounceTime(this.debounceTime),
+        filter(val => val.length >= this.minSearchLength)
+      )
+      .subscribe(searchString => {
+        this.searchApi
+          .requestSearchResult(searchString)
+          .subscribe(result => (this.searchResult = result));
+      });
   }
 }
--- src/app/components/search/search.module.ts
+++ src/app/components/search/search.module.ts
@@ -4,10 +4,18 @@ import { SearchComponent } from './search.component';
 import { SearchResultSectionComponent } from './components/search-result-section/search-result-section.component';
 import { ToGridItemsModule } from '../../pipes/to-grid-items/to-grid-items.module';
 import { UiCardsGridModule } from '../ui-cards-grid/ui-cards-grid.module';
+import { ReactiveFormsModule } from '@angular/forms';
+import { PreloaderModule } from '../preloader/preloader.module'; // import the ReactiveFormsModule
 
 @NgModule({
   declarations: [SearchComponent, SearchResultSectionComponent],
   exports: [SearchComponent],
-  imports: [CommonModule, ToGridItemsModule, UiCardsGridModule],
+  imports: [
+    ReactiveFormsModule,
+    CommonModule,
+    ToGridItemsModule,
+    UiCardsGridModule,
+    PreloaderModule,
+  ],
 })
 export class SearchModule {}
