e25c7471c00f8c125c6c799635282235afa4ccaf
Heliken <burshf@gmail.com>
2023-04-10 18:26:37 +0300

    remove unused import

--- src/app/components/preloader/preloader.module.ts
+++ src/app/components/preloader/preloader.module.ts
@@ -1,10 +1,9 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { PreloaderComponent } from './preloader.component';
 
 @NgModule({
   declarations: [PreloaderComponent],
   exports: [PreloaderComponent],
-  imports: [CommonModule],
+  imports: [],
 })
 export class PreloaderModule {}
