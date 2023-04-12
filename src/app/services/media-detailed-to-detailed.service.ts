8d03a5caf73c9cda41cc561842d78a4d8cf9a5f0
Heliken <burshf@gmail.com>
2023-04-12 21:01:02 +0300

    add service to transform data for detailed pages

---
+++ src/app/services/media-detailed-to-detailed.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core';
+
+import { MediaItemDetailed } from '../types/media-item';
+
+import { DetailedHero } from '../types/ui-types/detailed-hero';
+import { TvShowDetailed } from '../types/tv-show';
+import { MovieDetailed } from '../types/movie';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class MediaDetailedToDetailedService {
+  public convertToHero(media: MediaItemDetailed): DetailedHero {
+    return this.movieTvHeroMapper(media);
+  }
+
+  movieTvHeroMapper({
+    title,
+    mediaType,
+    releaseDate,
+    description,
+    rating,
+    genres,
+    poster,
+  }: TvShowDetailed | MovieDetailed): DetailedHero {
+    return {
+      img: poster,
+      title,
+      mediaType,
+      date: releaseDate,
+      description,
+      rating,
+      genres,
+    };
+  }
+}
