1bcbf418a204e078dd29c93758ac44ebdae19e7b
Heliken <burshf@gmail.com>
2023-04-12 21:05:03 +0300

    add detailed page hero to movie-tv-details page

--- src/app/pages/movie-tv-details/movie-tv-details.module.ts
+++ src/app/pages/movie-tv-details/movie-tv-details.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MovieTvDetailsComponent } from './movie-tv-details.component';
+import { DetailedPageHeroModule } from '../../components/detailed-page-hero/detailed-page-hero.module';
+import { ToDetailedHeroModule } from '../../pipes/to-detailed-hero/to-detailed-hero.module';
 
 @NgModule({
   declarations: [MovieTvDetailsComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, DetailedPageHeroModule, ToDetailedHeroModule],
 })
 export class MovieTvDetailsModule {}
