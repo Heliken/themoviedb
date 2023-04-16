2ad674f6a1412ba0d4db606de30a77a39c122c9c
Heliken <burshf@gmail.com>
2023-04-16 19:22:51 +0300

    remove unnecessary type guard

--- src/app/guards/detailed-page-type.guard.ts
+++
@@ -1,9 +0,0 @@
-import { ActivatedRouteSnapshot } from '@angular/router';
-import { MediaType } from '../types/media-type';
-
-export const detailedPageTypeGuard = (route: ActivatedRouteSnapshot) => {
-  return (
-    route.params['detailedPageType'] === MediaType.Movie ||
-    route.params['detailedPageType'] === MediaType.Tv
-  );
-};
