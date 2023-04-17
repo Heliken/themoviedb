f45d19a3b2b65ee0dbf63c817b1598eec517af91
Heliken <burshf@gmail.com>
2023-04-17 20:33:07 +0300

    add dropdown with rating picker; add ratingPostConfig input

--- src/app/components/detailed-page/components/detailed-page-hero-rating/detailed-page-hero-rating.component.ts
+++ src/app/components/detailed-page/components/detailed-page-hero-rating/detailed-page-hero-rating.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { RatingPostConfig } from 'src/app/pipes/to-rating-post-config/to-rating-post-config.pipe';
 
 @Component({
   selector: 'mdb-detailed-page-hero-rating',
@@ -8,6 +9,7 @@ import { Component, Input } from '@angular/core';
 export class DetailedPageHeroRatingComponent {
   @Input() public value = 0;
   @Input() public title = 'Rating: ';
+  @Input() public ratingPostConfig?: RatingPostConfig;
 
   public showDropdown = false;
 
