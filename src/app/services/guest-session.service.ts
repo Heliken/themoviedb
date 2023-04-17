11204e8099b878f8e4e8bfd3c08a09b7115a8c5d
Heliken <burshf@gmail.com>
2023-04-17 16:11:08 +0300

    add guest session sevice to get and store guest_session_id

---
+++ src/app/services/guest-session.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, map, of, tap } from 'rxjs';
+import { GuestSession } from '../types/guest-session';
+import { LocalStorageService } from './local-storage.service';
+import { GUEST_SESSION_CACHE_KEY } from 'src/api-info';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class GuestSessionService {
+  constructor(
+    private readonly http: HttpClient,
+    private localStorageService: LocalStorageService<string>
+  ) {}
+
+  public getSessionId(): Observable<string> {
+    const cachedSessionId = this.localStorageService.getItem(this.cacheKey);
+
+    if (cachedSessionId) {
+      return of(cachedSessionId);
+    } else {
+      return this.requestSessionId().pipe(
+        tap(guestSession => this.saveSessionId(guestSession)),
+        map(guestSession => guestSession.guest_session_id)
+      );
+    }
+  }
+
+  private cacheKey = GUEST_SESSION_CACHE_KEY;
+
+  private requestSessionId(): Observable<GuestSession> {
+    return this.http.get<GuestSession>('authentication/guest_session/new');
+  }
+
+  private saveSessionId({ guest_session_id, expires_at }: GuestSession): void {
+    this.localStorageService.setItem(this.cacheKey, {
+      data: guest_session_id,
+      timestamp: new Date(expires_at).getTime().toString(),
+    });
+  }
+}
