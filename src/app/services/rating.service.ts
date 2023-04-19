a08e8fc62fa6a9d73e559d8e4885f1a55e854fe7
Heliken <burshf@gmail.com>
2023-04-19 20:43:40 +0300

    add rating service to manage user ratings

---
+++ src/app/services/rating.service.ts
@@ -0,0 +1,72 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { MediaListResponse } from '../types/media-list-response';
+import { MovieDTORated } from '../types/DTO/movie-dto';
+import { BehaviorSubject, Observable, map, tap } from 'rxjs';
+import { RatedMap } from '../types/rated-map';
+import { CanBeRatedMediaType, MediaType } from '../types/media-type';
+import { GuestSessionService } from './guest-session.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class RatingService {
+  constructor(
+    private http: HttpClient,
+    private guestSessionService: GuestSessionService
+  ) {}
+
+  public ratedMovies$ = new BehaviorSubject<RatedMap>(
+    new Map<number, number>()
+  );
+
+  public ratedTvShows$ = new BehaviorSubject<RatedMap>(
+    new Map<number, number>()
+  );
+
+  public ratedMaps = {
+    [MediaType.Movie]: this.ratedMovies$,
+    [MediaType.Tv]: this.ratedTvShows$,
+  };
+
+  postRating(id: number, type: CanBeRatedMediaType, value: number) {
+    return this.http.post(
+      `${type}/${id}/rating?guest_session_id=${this.guestSessionService.getSessionId()}`,
+      {
+        value,
+      }
+    );
+  }
+
+  addNewRated(id: number, type: CanBeRatedMediaType, rating: number) {
+    const currentRatedMap = this.ratedMaps[type].getValue();
+    const newRatedMap = new Map(currentRatedMap).set(id, rating);
+    this.ratedMaps[type].next(newRatedMap);
+  }
+
+  requestRatedMovies(sessionId: string): Observable<RatedMap> {
+    return this.http
+      .get<MediaListResponse<MovieDTORated>>(
+        `guest_session/${sessionId}/rated/movies`
+      )
+      .pipe(
+        map(({ results }) => this.convertToMap(results)),
+        tap(resultsMap => this.ratedMovies$.next(resultsMap))
+      );
+  }
+
+  requestRatedTvShows(sessionId: string): Observable<RatedMap> {
+    return this.http
+      .get<MediaListResponse<MovieDTORated>>(
+        `guest_session/${sessionId}/rated/tv`
+      )
+      .pipe(
+        map(({ results }) => this.convertToMap(results)),
+        tap(resultsMap => this.ratedTvShows$.next(resultsMap))
+      );
+  }
+
+  private convertToMap(movies: MovieDTORated[]): Map<number, number> {
+    return new Map(movies.map(({ id, rating }) => [id, rating]));
+  }
+}
