5fc0bae73b6a472b9570f490e32a5de4bbb1c77e
Heliken <burshf@gmail.com>
2023-04-19 20:44:22 +0300

    add logic to ratingPicker

--- src/app/components/rating-picker/rating-picker.component.ts
+++ src/app/components/rating-picker/rating-picker.component.ts
@@ -1,5 +1,9 @@
 import { Component, Input } from '@angular/core';
 import { FormControl } from '@angular/forms';
+import { BehaviorSubject, switchMap } from 'rxjs';
+import { RatingPostConfig } from 'src/app/pipes/to-rating-post-config/to-rating-post-config.pipe';
+import { GuestSessionService } from 'src/app/services/guest-session.service';
+import { RatingService } from 'src/app/services/rating.service';
 
 @Component({
   selector: 'mdb-rating-picker',
@@ -7,11 +11,42 @@ import { FormControl } from '@angular/forms';
   styleUrls: ['./rating-picker.component.scss'],
 })
 export class RatingPickerComponent {
-  @Input() public initial = 5;
+  constructor(
+    private readonly ratingService: RatingService,
+    private readonly guestSessionService: GuestSessionService
+  ) {}
 
-  public min = 0;
+  @Input() public initial = 5;
+  @Input() public ratingPostConfig?: RatingPostConfig;
+
+  public isSubmitting$ = new BehaviorSubject<boolean>(false);
+  public step = 0.5;
+  public min = this.step;
   public max = 10;
-  public step = 1;
+  public ratedMaps = this.ratingService.ratedMaps;
 
   public ratingValue = new FormControl(this.initial);
+
+  public submitRating() {
+    if (this.ratingPostConfig && this.ratingValue.value) {
+      const { id, type } = this.ratingPostConfig;
+      const value = this.ratingValue.value;
+
+      this.isSubmitting$.next(true);
+
+      this.guestSessionService
+        .loadSessionId()
+        .pipe(switchMap(() => this.ratingService.postRating(id, type, value)))
+        .subscribe({
+          next: () => {
+            this.ratingService.addNewRated(id, type, value);
+            this.isSubmitting$.next(false);
+          },
+          error: error => {
+            console.error('Error submitting rating:', error);
+            this.isSubmitting$.next(false);
+          },
+        });
+    }
+  }
 }
--- src/app/components/rating-picker/rating-picker.module.ts
+++ src/app/components/rating-picker/rating-picker.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RatingPickerComponent } from './rating-picker.component';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MapGetModule } from '../../pipes/map-get/map-get.module';
 
 @NgModule({
   declarations: [RatingPickerComponent],
   exports: [RatingPickerComponent],
-  imports: [CommonModule, ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, MapGetModule],
 })
 export class RatingPickerModule {}
