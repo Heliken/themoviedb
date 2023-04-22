634eaa9ed91ede205e8447abad8640f79407279d
Heliken <burshf@gmail.com>
2023-04-22 22:36:20 +0300

    build person details hero block

--- src/app/pages/person-details/person-details.component.ts
+++ src/app/pages/person-details/person-details.component.ts
@@ -1,8 +1,34 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { BehaviorSubject, tap, switchMap } from 'rxjs';
+import { popularityFormula } from 'src/app/helpers/popularity-formula';
+import { SortFunc } from 'src/app/pipes/sort/sort.pipe';
+import { DetailedDataAPIService } from 'src/app/services/detailed-data-api.service';
+import { KnownFor } from 'src/app/types/person';
 
 @Component({
   selector: 'mdb-person-details',
   templateUrl: './person-details.component.html',
   styleUrls: ['./person-details.component.scss'],
 })
-export class PersonDetailsComponent {}
+export class PersonDetailsComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private detailedDataService: DetailedDataAPIService
+  ) {}
+
+  public isLoading$ = new BehaviorSubject<boolean>(true);
+
+  public knownForToShow = 8;
+
+  public knownForSortFunc: SortFunc<KnownFor> = (a, b) =>
+    popularityFormula(b) - popularityFormula(a);
+
+  public details$ = this.route.params.pipe(
+    tap(() => this.isLoading$.next(true)),
+    switchMap(params =>
+      this.detailedDataService.requestPersonDetailedData(params['id'])
+    ),
+    tap(() => this.isLoading$.next(false))
+  );
+}
--- src/app/pages/person-details/person-details.module.ts
+++ src/app/pages/person-details/person-details.module.ts
@@ -1,10 +1,26 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PersonDetailsComponent } from './person-details.component';
+import { DetailedPageModule } from '../../components/detailed-page/detailed-page.module';
+import { PreloaderModule } from '../../components/preloader/preloader.module';
+import { UiCardModule } from 'src/app/components/ui-card/ui-card.module';
+import { UiCardsGridModule } from '../../components/ui-cards-grid/ui-cards-grid.module';
+import { ToGridItemsModule } from '../../pipes/to-grid-items/to-grid-items.module';
+import { ToKnownForModule } from '../../pipes/to-known-for/to-known-for.module';
+import { SortModule } from '../../pipes/sort/sort.module';
 
 @NgModule({
   declarations: [PersonDetailsComponent],
   exports: [PersonDetailsComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    DetailedPageModule,
+    PreloaderModule,
+    UiCardModule,
+    UiCardsGridModule,
+    ToGridItemsModule,
+    ToKnownForModule,
+    SortModule,
+  ],
 })
 export class PersonDetailsModule {}
