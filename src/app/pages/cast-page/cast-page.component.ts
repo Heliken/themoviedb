a6bd50501b11061132ba99c8fb3849f58520abb6
Heliken <burshf@gmail.com>
2023-04-23 19:05:08 +0300

    build cast page layout

--- src/app/pages/cast-page/cast-page.component.ts
+++ src/app/pages/cast-page/cast-page.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {
+  BehaviorSubject,
+  tap,
+  switchMap,
+  Observable,
+  combineLatest,
+} from 'rxjs';
+import { DetailedDataAPIService } from 'src/app/services/detailed-data-api.service';
+import { CastPageRouteData } from 'src/app/types/cast-page-route-data';
+import { MediaType } from 'src/app/types/media-type';
 @Component({
   selector: 'mdb-cast-page',
   templateUrl: './cast-page.component.html',
   styleUrls: ['./cast-page.component.scss'],
 })
-export class CastPageComponent {}
+export class CastPageComponent {
+  constructor(
+    private route: ActivatedRoute,
+    private detailedDataService: DetailedDataAPIService
+  ) {}
+
+  public isLoading$ = new BehaviorSubject<boolean>(true);
+
+  public details$ = combineLatest([
+    this.route.params,
+    this.route.data as Observable<CastPageRouteData>,
+  ]).pipe(
+    tap(() => this.isLoading$.next(true)),
+    switchMap(([params, data]) => {
+      const id = params['id'];
+      const mediaType = data['mediaType'];
+
+      switch (mediaType) {
+        case MediaType.Movie:
+          return this.detailedDataService.requestMovieDetailedData(id);
+        case MediaType.Tv:
+          return this.detailedDataService.requestTvShowDetailedData(id);
+      }
+    }),
+    tap(() => this.isLoading$.next(false))
+  );
+}
--- src/app/pages/cast-page/cast-page.module.ts
+++ src/app/pages/cast-page/cast-page.module.ts
@@ -1,9 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CastPageComponent } from './cast-page.component';
+import { PreloaderModule } from '../../components/preloader/preloader.module';
+import { UiCardsGridModule } from '../../components/ui-cards-grid/ui-cards-grid.module';
+import { ToGridItemsModule } from '../../pipes/to-grid-items/to-grid-items.module';
+import { ConcatModule } from '../../pipes/concat/concat.module';
+import { GroupByModule } from '../../pipes/group-array-by/group-array-by.module';
 
 @NgModule({
   declarations: [CastPageComponent],
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    PreloaderModule,
+    UiCardsGridModule,
+    ToGridItemsModule,
+    ConcatModule,
+    GroupByModule,
+  ],
 })
 export class CastPageModule {}
