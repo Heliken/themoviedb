f4f5f3364e11c91d244df95ddb8f560e3dd7eaa9
Heliken <burshf@gmail.com>
2023-04-23 12:09:46 +0300

    rework groupArrayBy pipe

--- src/app/pipes/group-array-by/group-array-by.pipe.ts
+++ src/app/pipes/group-array-by/group-array-by.pipe.ts
@@ -1,26 +1,26 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 export interface GroupedValues<T> {
-  [key: string]: T[];
+  key: string;
+  value: T[];
 }
 
 @Pipe({
   name: 'groupArrayBy',
 })
 export class GroupArrayByPipe<T> implements PipeTransform {
-  transform(items: T[], key: keyof T): GroupedValues<T> {
-    return items.reduce((acc: GroupedValues<T>, val: T) => {
+  transform(items: T[], key: keyof T): GroupedValues<T>[] {
+    return items.reduce((acc: GroupedValues<T>[], val: T) => {
       const keyValue = val[key];
-      if (typeof keyValue !== 'string') {
-        throw new Error(
-          `Expected ${key.toString()} to be a string, but got ${typeof keyValue}`
-        );
-      }
-      let keyGroup = acc[keyValue];
+      const keyGroup = acc.find(({ key }) => key === keyValue);
 
-      keyGroup ? keyGroup.push(val) : (keyGroup = [val]);
-
+      keyGroup
+        ? keyGroup.value.push(val)
+        : acc.push({
+            key: keyValue as string,
+            value: [val],
+          });
       return acc;
-    }, {});
+    }, []);
   }
 }
