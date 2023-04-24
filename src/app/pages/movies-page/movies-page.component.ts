59beb895f4b462e1ca23e484dcdf18f27ee15776
Heliken <burshf@gmail.com>
2023-04-24 19:57:11 +0300

    add route to movies spage, adn movies page module

---
+++ src/app/pages/movies-page/movies-page.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'mdb-movies-page',
+  templateUrl: './movies-page.component.html',
+  styleUrls: ['./movies-page.component.scss'],
+})
+export class MoviesPageComponent {}
---
+++ src/app/pages/movies-page/movies-page.module.ts
@@ -0,0 +1,9 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MoviesPageComponent } from './movies-page.component';
+
+@NgModule({
+  declarations: [MoviesPageComponent],
+  imports: [CommonModule],
+})
+export class MoviesPageModule {}
