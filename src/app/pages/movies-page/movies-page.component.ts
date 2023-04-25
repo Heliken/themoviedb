aef0bd88db80c3e4cb855f8edf6f10fdab4ee6c0
Heliken <burshf@gmail.com>
2023-04-25 22:34:57 +0300

    use formGroup in movies-page component

--- src/app/pages/movies-page/movies-page.component.ts
+++ src/app/pages/movies-page/movies-page.component.ts
@@ -1,11 +1,11 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { MoviesPageApiService } from './movies-page-api.service';
 import { BehaviorSubject, Subscription, switchMap, tap } from 'rxjs';
 import { UICardsGridType } from '../../types/ui-types/ui-cards-grid-type';
 import { SortType } from './types/sort-types';
 import { defaultSortOption, sortingOptions } from './configs/sort-config';
-import { FormControl } from '@angular/forms';
+import { FormBuilder, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'mdb-movies-page',
@@ -16,60 +16,52 @@ export class MoviesPageComponent implements OnInit, OnDestroy {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
-    private moviesApiService: MoviesPageApiService
+    private moviesApiService: MoviesPageApiService,
+    private formBuilder: FormBuilder
   ) {}
+
+  public controlsFormGroup = this.formBuilder.group({
+    sort_by: new FormControl<SortType>(defaultSortOption),
+    page: new FormControl<number>(1),
+  });
 
   public fullpageGrid = UICardsGridType.fullpage;
   public sortingOptions = sortingOptions;
-  public sortFormControl = new FormControl<SortType | null>(defaultSortOption);
 
   public isLoading$ = new BehaviorSubject<boolean>(true);
 
-  private sortSubscription?: Subscription;
+  private controlsSubscription?: Subscription;
 
-  public setPage(newPage: number): void {
+  public setPage(page: number): void {
     window.scrollTo({ top: 0, behavior: 'smooth' });
 
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: { page: newPage },
-      queryParamsHandling: 'merge',
-    });
-  }
-
-  public setSorting(sort_by: SortType | null): void {
-    this.router.navigate([], {
-      relativeTo: this.route,
-      queryParams: {
-        sort_by,
-        page: 1,
-      },
-      queryParamsHandling: 'merge',
-    });
+    this.controlsFormGroup.patchValue({ page: page });
   }
 
   public results$ = this.route.queryParams.pipe(
     tap(() => this.isLoading$.next(true)),
-    tap(params => this.setSortFormControlValue(params['sort_by'])),
+    tap(params => this.controlsFormGroup.patchValue(params)),
     switchMap(params => this.moviesApiService.requestMovies(params)),
     tap(() => this.isLoading$.next(false))
   );
 
-  public setSortFormControlValue(sortType: SortType | null): void {
-    if (sortType && sortType !== this.sortFormControl.value) {
-      this.sortFormControl.patchValue(sortType);
-    }
+  public ngOnInit(): void {
+    this.controlsSubscription = this.controlsFormGroup.valueChanges.subscribe(
+      params => this.setQueryParams(params)
+    );
   }
 
-  public ngOnInit(): void {
-    this.sortSubscription = this.sortFormControl.valueChanges.subscribe(res => {
-      this.setSorting(res);
+  public setQueryParams(params: Params) {
+    this.router.navigate([], {
+      relativeTo: this.route,
+      queryParams: params,
+      queryParamsHandling: 'merge',
     });
   }
 
   public ngOnDestroy(): void {
-    if (this.sortSubscription) {
-      this.sortSubscription.unsubscribe();
+    if (this.controlsSubscription) {
+      this.controlsSubscription.unsubscribe();
     }
   }
 }
--- src/app/pages/movies-page/movies-page.module.ts
+++ src/app/pages/movies-page/movies-page.module.ts
@@ -6,6 +6,7 @@ import { PreloaderModule } from '../../components/preloader/preloader.module';
 import { UiCardsGridModule } from '../../components/ui-cards-grid/ui-cards-grid.module';
 import { ToGridItemsModule } from '../../pipes/to-grid-items/to-grid-items.module';
 import { SelectModule } from '../../components/select/select.module';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [MoviesPageComponent],
@@ -16,6 +17,7 @@ import { SelectModule } from '../../components/select/select.module';
     UiCardsGridModule,
     ToGridItemsModule,
     SelectModule,
+    ReactiveFormsModule,
   ],
 })
 export class MoviesPageModule {}
