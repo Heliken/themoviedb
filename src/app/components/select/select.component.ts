48781a4bf1d25405a485968de6310b994e146ba7
Heliken <burshf@gmail.com>
2023-04-26 19:23:00 +0300

    add eventEmitter to select to pass what selection change should trigger

--- src/app/components/select/select.component.ts
+++ src/app/components/select/select.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { FormControl } from '@angular/forms';
 import { SelectOption } from '../../types/ui-types/select-option';
 
@@ -11,4 +11,12 @@ export class SelectComponent {
   @Input() public title?: string;
   @Input() public options: SelectOption[] = [];
   @Input() public selectControl?: FormControl;
+  @Output() public selectChange = new EventEmitter<string>();
+
+  public selectOption(event: Event) {
+    const selectedValue = (event.target as HTMLSelectElement).value;
+    if (selectedValue) {
+      this.selectChange.emit(selectedValue);
+    }
+  }
 }
