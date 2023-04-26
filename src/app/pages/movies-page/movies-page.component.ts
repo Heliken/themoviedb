17263c7f504af11f100adf949234fb9a7fc765d6
Heliken <burshf@gmail.com>
2023-04-26 19:37:02 +0300

    rework component to subscribe only on queryParams changes, use formGroup values only to update UI

--- src/app/pages/movies-page/movies-page.component.ts
+++ src/app/pages/movies-page/movies-page.component.ts
@@ -1,7 +1,7 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { ActivatedRoute, Params, Router } from '@angular/router';
 import { MoviesPageApiService } from './movies-page-api.service';
-import { BehaviorSubject, Subscription, switchMap, tap } from 'rxjs';
+import { BehaviorSubject, switchMap, tap } from 'rxjs';
 import { UICardsGridType } from '../../types/ui-types/ui-cards-grid-type';
 import { SortType } from './types/sort-types';
 import { defaultSortOption, sortingOptions } from './configs/sort-config';
@@ -12,7 +12,7 @@ import { FormBuilder, FormControl } from '@angular/forms';
   templateUrl: './movies-page.component.html',
   styleUrls: ['./movies-page.component.scss'],
 })
-export class MoviesPageComponent implements OnInit, OnDestroy {
+export class MoviesPageComponent {
   constructor(
     private router: Router,
     private route: ActivatedRoute,
@@ -20,6 +20,7 @@ export class MoviesPageComponent implements OnInit, OnDestroy {
     private formBuilder: FormBuilder
   ) {}
 
+  // used only to store values for UI
   public controlsFormGroup = this.formBuilder.group({
     sort_by: new FormControl<SortType>(defaultSortOption),
     page: new FormControl<number>(1),
@@ -30,26 +31,24 @@ export class MoviesPageComponent implements OnInit, OnDestroy {
 
   public isLoading$ = new BehaviorSubject<boolean>(true);
 
-  private controlsSubscription?: Subscription;
-
   public setPage(page: number): void {
-    window.scrollTo({ top: 0, behavior: 'smooth' });
-
-    this.controlsFormGroup.patchValue({ page: page });
+    this.setQueryParams({ page });
   }
 
+  public setSorting(sort_by: string): void {
+    this.setQueryParams({ page: undefined, sort_by });
+  }
+
+  // used as source of state of the page, only patches value to formGroup to correctly update UI
   public results$ = this.route.queryParams.pipe(
     tap(() => this.isLoading$.next(true)),
-    tap(params => this.controlsFormGroup.patchValue(params)),
+    tap(params => {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+      this.controlsFormGroup.patchValue(params);
+    }),
     switchMap(params => this.moviesApiService.requestMovies(params)),
     tap(() => this.isLoading$.next(false))
   );
-
-  public ngOnInit(): void {
-    this.controlsSubscription = this.controlsFormGroup.valueChanges.subscribe(
-      params => this.setQueryParams(params)
-    );
-  }
 
   public setQueryParams(params: Params) {
     this.router.navigate([], {
@@ -57,11 +56,5 @@ export class MoviesPageComponent implements OnInit, OnDestroy {
       queryParams: params,
       queryParamsHandling: 'merge',
     });
-  }
-
-  public ngOnDestroy(): void {
-    if (this.controlsSubscription) {
-      this.controlsSubscription.unsubscribe();
-    }
   }
 }
