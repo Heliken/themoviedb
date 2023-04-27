a3e323f55b81c94079a81647bd5c9fe8ad15b8a4
Heliken <burshf@gmail.com>
2023-04-27 22:26:13 +0300

    rework pagination pipe

--- src/app/components/pagination/pagination.pipe.ts
+++ src/app/components/pagination/pagination.pipe.ts
@@ -1,42 +1,96 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
+export enum PaginationProgress {
+  start = 'start',
+  default = 'default',
+  end = 'end',
+}
+
 @Pipe({
   name: 'pagination',
 })
 export class PaginationPipe implements PipeTransform {
-  private pagesToShow = 7;
+  private pagesToShow = 7; // amount of elements to show
+  private paginationStart = 1;
+  private edgePageThreshold = 3; // number that determines edge states of pagination current page
 
+  // populate array of passed size starting with pased
   getFilledArray(start: number, size: number) {
     return [...Array(size).keys()].map(i => i + start);
+  }
+
+  getPaginationPattern(
+    paginationProgress: PaginationProgress,
+    current: number,
+    total: number
+  ) {
+    switch (paginationProgress) {
+      case PaginationProgress.start:
+        return [
+          this.paginationStart,
+          ...this.getFilledArray(
+            this.paginationStart + 1,
+            this.edgePageThreshold + 1
+          ),
+          undefined,
+          total,
+        ];
+      case PaginationProgress.end:
+        return [
+          this.paginationStart,
+          undefined,
+          ...this.getFilledArray(
+            total - this.edgePageThreshold - 1,
+            this.edgePageThreshold + 1
+          ),
+          total,
+        ];
+      default:
+        return [
+          this.paginationStart,
+          undefined,
+          ...this.getFilledArray(current - 1, this.edgePageThreshold),
+          undefined,
+          total,
+        ];
+    }
   }
 
   transform(
     totalPages: number,
     currentPage: number
   ): Array<number | undefined> {
+    const startEdgePage = this.pagesToShow - this.edgePageThreshold;
+    const endEdgePage = totalPages - this.edgePageThreshold;
+
+    // low amount of pages
     if (totalPages <= this.pagesToShow) {
       return this.getFilledArray(1, totalPages);
     }
 
-    if (currentPage <= this.pagesToShow - 4) {
-      return [1, ...this.getFilledArray(2, 4), undefined, totalPages];
+    // active at the start of pagination
+    if (currentPage <= startEdgePage) {
+      return this.getPaginationPattern(
+        PaginationProgress.start,
+        currentPage,
+        totalPages
+      );
     }
 
-    if (currentPage > totalPages - 4) {
-      return [
-        1,
-        undefined,
-        ...this.getFilledArray(totalPages - 4, 4),
-        totalPages,
-      ];
+    // active at the end of pagination
+    if (currentPage >= endEdgePage) {
+      return this.getPaginationPattern(
+        PaginationProgress.end,
+        currentPage,
+        totalPages
+      );
     }
 
-    return [
-      1,
-      undefined,
-      ...this.getFilledArray(currentPage - 1, 3),
-      undefined,
-      totalPages,
-    ];
+    // active in the middle of pagination
+    return this.getPaginationPattern(
+      PaginationProgress.default,
+      currentPage,
+      totalPages
+    );
   }
 }
