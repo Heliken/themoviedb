c9f3e8b0a7c9cd38c6256ceee61068daaa75e2dd
Heliken <burshf@gmail.com>
2023-04-30 17:32:41 +0300

    additional styles to header-login component

--- src/app/components/header/header.module.ts
+++ src/app/components/header/header.module.ts
@@ -3,11 +3,11 @@ import { HeaderComponent } from './header.component';
 import { SearchModule } from '../search/search.module';
 import { HeaderLoginComponent } from './components/header-login/header-login.component';
 import { CommonModule } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @NgModule({
   declarations: [HeaderComponent, HeaderLoginComponent],
   exports: [HeaderComponent],
-  imports: [SearchModule, CommonModule, RouterLink],
+  imports: [SearchModule, CommonModule, RouterLink, RouterLinkActive],
 })
 export class HeaderModule {}
